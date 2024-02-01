97f2c4780e8424003b53fb9433389ae5f1754d8c
hena-ndombele <henandombele2021@gmail.com>
2024-02-01 18:48:19 +0100

    Feat: Ajout de different menu dans le drawer

--- lib/demarrage/HomePage.dart
+++ lib/demarrage/HomePage.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:boutique_app/utils/ColorPage.dart';
 import 'package:boutique_app/widgets/CardWidget.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -12,31 +11,84 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Ma Boutique", style: TextStyle(color: ColorPages.COLOR_BLANCHE, fontWeight: FontWeight.bold),),
+        title: Text(
+          "Magasin Stock",
+          style: TextStyle(
+              color: ColorPages.COLOR_BLANCHE, fontWeight: FontWeight.bold),
+        ),
         backgroundColor: ColorPages.COLOR_PRINCIPAL,
       ),
       body: Column(
         children: [
-          Row(
-            children: [
-             CardWidget(
-                 onPressed: (){},
-                 text: "Magasins",
+          Container(
+            child: Row(
+              children: [
+                CardWidget(
+                  onPressed: () {},
+                  icon: Icons.home,
+                  text: "Marchandises",
+                  text1: "278",
+                ),
+                SizedBox(width: 10),
+                CardWidget(
+                  onPressed: () {},
+                  icon: Icons.monetization_on,
+                  text: "Dépenses",
+                  text1: "",
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              children: [
+                CardWidget(
+                  onPressed: () {},
+                  icon: Icons.input,
+                  text: "Entrée Stock",
+                  text1: "78",
+                ),
+                SizedBox(width: 10),
+                CardWidget(
+                  onPressed: () {},
+                  icon: Icons.output,
+                  text: "Sortie Stock",
+                  text1: "28",
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              children: [
+                CardWidget(
+                  onPressed: () {},
+                  icon: Icons.rate_review,
+                  text: "Rapport",
+                  text1: "",
+                ),
+                SizedBox(width: 10),
+                CardWidget(
+                  onPressed: () {},
+                  icon: Icons.call,
+                  text: "Contactez-nous",
+                  text1: "",
+                ),
+              ],
+            ),
+          ),
 
-             )
-            ],
-          )
+
+
+
+
         ],
       ),
-      
       drawer: Drawer(
         child: Container(
           child: SingleChildScrollView(
             child: Column(
-              children: [
-               EnteteMenu(),
-                ListeMenu()
-              ],
+              children: [EnteteMenu(), ListeMenu()],
             ),
           ),
         ),
--- lib/drawer/ListeMenu.dart
+++ lib/drawer/ListeMenu.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:boutique_app/utils/ColorPage.dart';
 
-
 class ListeMenu extends StatefulWidget {
   const ListeMenu({super.key});
 
@@ -19,106 +18,152 @@ class _ListeMenuState extends State<ListeMenu> {
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
-              Icon(Icons.home_filled, size: 20, color: ColorPages.COLOR_PRINCIPAL,),
+                Icon(
+                  Icons.home_filled,
+                  size: 20,
+                  color: ColorPages.COLOR_PRINCIPAL,
+                ),
                 Expanded(
                   child: ListTile(
                     title: Text('Magasin'),
-                    onTap: (){},
+                    onTap: () {},
                   ),
-                ),],
+                ),
+              ],
             ),
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
-                Icon(Icons.dashboard, size: 20, color: ColorPages.COLOR_PRINCIPAL,),
+                Icon(
+                  Icons.dashboard,
+                  size: 20,
+                  color: ColorPages.COLOR_PRINCIPAL,
+                ),
                 Expanded(
                   child: ListTile(
                     title: Text('Marchandises'),
-                    onTap: (){},
+                    onTap: () {},
                   ),
-                ),],
+                ),
+              ],
             ),
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
-                Icon(Icons.rate_review, size: 20, color: ColorPages.COLOR_PRINCIPAL,),
+                Icon(
+                  Icons.rate_review,
+                  size: 20,
+                  color: ColorPages.COLOR_PRINCIPAL,
+                ),
                 Expanded(
                   child: ListTile(
                     title: Text('Rapports'),
-                    onTap: (){},
+                    onTap: () {},
                   ),
-                ),],
+                ),
+              ],
             ),
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
-                Icon(Icons.monetization_on, size: 20, color: ColorPages.COLOR_PRINCIPAL,),
+                Icon(
+                  Icons.monetization_on,
+                  size: 20,
+                  color: ColorPages.COLOR_PRINCIPAL,
+                ),
                 Expanded(
                   child: ListTile(
                     title: Text('Dépenses'),
-                    onTap: (){},
+                    onTap: () {},
                   ),
-                ),],
+                ),
+              ],
             ),
           ),
-          Divider(height: 2,color: ColorPages.COLOR_PRINCIPAL,),
+          Divider(
+            height: 2,
+            color: ColorPages.COLOR_PRINCIPAL,
+          ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
-                Icon(Icons.person_remove, size: 20, color: ColorPages.COLOR_PRINCIPAL,),
+                Icon(
+                  Icons.person_remove,
+                  size: 20,
+                  color: ColorPages.COLOR_PRINCIPAL,
+                ),
                 Expanded(
                   child: ListTile(
                     title: Text('Fournisseurs'),
-                    onTap: (){},
+                    onTap: () {},
                   ),
-                ),],
+                ),
+              ],
             ),
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
-                Icon(Icons.person_add, size: 20, color: ColorPages.COLOR_PRINCIPAL,),
+                Icon(
+                  Icons.person_add,
+                  size: 20,
+                  color: ColorPages.COLOR_PRINCIPAL,
+                ),
                 Expanded(
                   child: ListTile(
                     title: Text('Clients'),
-                    onTap: (){},
+                    onTap: () {},
                   ),
-                ),],
+                ),
+              ],
             ),
           ),
-          Divider(height: 2,color: ColorPages.COLOR_PRINCIPAL,),
+          Divider(
+            height: 2,
+            color: ColorPages.COLOR_PRINCIPAL,
+          ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
-                Icon(Icons.help, size: 20, color: ColorPages.COLOR_PRINCIPAL,),
+                Icon(
+                  Icons.help,
+                  size: 20,
+                  color: ColorPages.COLOR_PRINCIPAL,
+                ),
                 Expanded(
                   child: ListTile(
                     title: Text('Aide'),
-                    onTap: (){},
+                    onTap: () {},
                   ),
-                ),],
+                ),
+              ],
             ),
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
-                Icon(Icons.receipt_long, size: 20, color: ColorPages.COLOR_PRINCIPAL,),
+                Icon(
+                  Icons.receipt_long,
+                  size: 20,
+                  color: ColorPages.COLOR_PRINCIPAL,
+                ),
                 Expanded(
                   child: ListTile(
                     title: Text('Politique de confidentialité'),
-                    onTap: (){},
+                    onTap: () {},
                   ),
-                ),],
+                ),
+              ],
             ),
           ),
           Container(
@@ -126,17 +171,25 @@ class _ListeMenuState extends State<ListeMenu> {
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
-                Icon(Icons.exit_to_app, size: 20, color: ColorPages.COLOR_BLANCHE,),
+                Icon(
+                  Icons.exit_to_app,
+                  size: 20,
+                  color: ColorPages.COLOR_BLANCHE,
+                ),
                 Expanded(
                   child: ListTile(
-                    title: Text('Deconnexion', style: TextStyle(color: ColorPages.COLOR_BLANCHE,fontWeight: FontWeight.bold),),
-                    onTap: (){},
+                    title: Text(
+                      'Deconnexion',
+                      style: TextStyle(
+                          color: ColorPages.COLOR_BLANCHE,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onTap: () {},
                   ),
-                ),],
+                ),
+              ],
             ),
           ),
-
-
         ],
       ),
     );
